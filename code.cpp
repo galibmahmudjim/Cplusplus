d887f4e1c00ce7d6c46b507b61bde7911abbd007
galibmahmudjim <galibmahmudjim@gmail.com>
2021-03-27 04:21:09 +0600

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+//...............................
+#ifdef LOCAL
+#include "debugger.h"
+#else
+#define debug(arrags...)
+#endif
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define space ' '
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+#define testcase int tc; cin >> tc; while(tc--)
+#define input freopen("input.txt","r",stdin)
+#define output freopen("output.txt","w",stdout);
+#define pb push_back
+#define loop(i,a,n) for(int i = a; i <=n; i++)
+#define ii  long long int
+#define MAX 100000
+#define inf 10000000
+
+//...............................
+
+}
+int main()
+{
+     ios_base::sync_with_stdio(false); cin.tie(NULL);
+     #ifdef LOCAL
+     input;
+     #endif
+     testcase
+     solve();
+     return 0;
+
+}
